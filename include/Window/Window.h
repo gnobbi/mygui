8f0d5662772e21d621fc2a5c1d7f6ba0427f7c0e
gnobbi <github@danielnicklaus.info>
2016-11-28 19:41:01 +0100

    Initial CMake project finished

---
+++ include/Window/Window.h
@@ -0,0 +1,12 @@
+#ifndef MY_GUI_WINDOW_WINDOW_H
+#define MY_GUI_WINDOW_WINDOW_H
+
+#include "common/DllExportMacro.h"
+
+
+class DLLEXPORT Window {
+public:
+	Window() {};
+	Window(int argc, char *argv[]);
+};
+#endif //MY_GUI_WINDOW_WINDOW_H
---
+++ src/window/Window.cpp
@@ -0,0 +1,18 @@
+#include "Window/Window.h"
+#include <iostream>
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QApplication>
+
+
+void Dummy() { Window x; };
+
+
+Window::Window(int argc, char *argv[]) 
+{
+	QApplication app(argc, argv);
+	std::cout << "Window Ctor" << std::endl;
+	QWidget s;
+	s.show();
+	app.exec();
+}
+
